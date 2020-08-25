ac6f5de1e5c1e3c6339da99a9d2b34e15764ba3c
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-08-25 11:37:45 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid">
+      <header>Header</header>
+      <aside>Left column</aside>
+      <main>
+        <div>Body</div>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
